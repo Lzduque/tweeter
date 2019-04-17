837ac1c8a6804efff3a8631c8a92317985e580a1
Lzduque <lzduque@hotmail.com>
2019-04-17 19:06:15 +0000

    disallow form submission in the event that the tweet area is empty or exceeds the 140 character limit

--- public/scripts/tweet-hover.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $("article.tweet").hover(function() {
-    $("article.tweet").toggleClass("tweet-hover");
-    $("p.login-name").toggleClass("login-name-hover");
-    $("img.icons").toggleClass("icons-hover");
-  });
-});
