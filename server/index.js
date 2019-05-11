41bc0080b373040991fead598d83a95afa2d0dae
Lzduque <lzduque@hotmail.com>
2019-05-11 17:59:24 +0000

    Fix mongo connection string issue - solve problems with database

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
 const MongoClient   = require("mongodb").MongoClient;
-const MONGODB_URI   = process.env.MONGODB_URI || "mongodb://localhost:27017/tweeter";
+const MONGODB_URI   = process.env.MONGOLAB_BLUE_URI || "mongodb://localhost:27017/tweeter";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
